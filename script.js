78d2eca56f474cae15e0e7c0feef93115d051b40
Sakshi Gaikwad <123818553+sakshig03@users.noreply.github.com>
2025-02-27 20:05:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Code for Smooth Scrolling and Dynamic Effects
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
